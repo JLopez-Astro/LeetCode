696e1f752c0ff58b01318cde1476fe80cb013eae
John Lopez <81821708+JLopez-Astro@users.noreply.github.com>
2023-04-24 21:54:26 -0700

    Time: 281 ms (45.73%), Space: 14 MB (33.54%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -3,7 +3,6 @@ class Solution:
         amount = []
         for i in nums:
             track = []
-            # track.append(j < i for j in nums)
             for j in nums:
                 if j < i:
                     track.append(j)
